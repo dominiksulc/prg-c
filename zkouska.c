604d7509d29af62b8eb3016873236b6282843464
dominiksulc <76943171+dominiksulc@users.noreply.github.com>
2021-04-30 15:53:40 +0200

    Create zkouska.c

---
+++ zkouska.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {    
+
+    int cislo1, cislo2, sum;
+    
+    printf("s: ");
+    scanf("%d %d", &1, &2);
+
+    // vypocita sumu
+    sum = 1 + 2;      
+    
+    printf("%d + %d = %d", cislo1, cislo2, sum);
+    return 0;
+}
